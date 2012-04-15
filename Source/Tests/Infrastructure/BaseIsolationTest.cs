876090dd4b1a690ce138f49924e88b4d17c7bec1
Joe Shipley <jshipley@madmobile.com>
2012-04-15 08:53:01 -0400

    Adding testing classes for isolation testing, leaving out integration setup for now. Using MSpec, Moq, AutoMoq.

---
+++ Source/Tests/Infrastructure/BaseIsolationTest.cs
@@ -0,0 +1,11 @@
+﻿using AutoMoq;
+
+namespace TTT.Tests.Infrastructure
+{
+	public class BaseIsolationTest<T>
+		where T : class
+	{
+		protected readonly static AutoMoqer Mocks = new AutoMoqer();
+		protected static T ClassUnderTest = Mocks.Resolve<T>();
+	}
+}
---
+++ Source/Tests/Unit/Core/Application/Services/GameServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Machine.Specifications;
+using TTT.Core.Application.Services;
+using TTT.Tests.Infrastructure;
+
+namespace TTT.Tests.Unit.Core.Application.Services.GameServiceTests
+{
+	[Subject("Application, Services, GameService, Testing the testing classes")]
+	public class When_attempting_to_setup_the_testing_classes
+		: BaseIsolationTest<GameService>
+	{
+		private static bool _result;
+
+		Establish context = () => 
+		{
+		};
+
+		Because of = () =>
+		{
+			_result = ClassUnderTest.Test();
+		};
+
+		It should_be_all_good = () =>
+			_result.ShouldBeTrue();
+	}
+}

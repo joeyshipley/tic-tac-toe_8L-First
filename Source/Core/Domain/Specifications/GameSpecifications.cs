7b4e0d50913618113083014dc700244be95bf593
Joe Shipley <jshipley@madmobile.com>
2012-04-15 20:41:19 -0400

    Implementing GameSpecifications.IsMoveLegitimate.

--- Source/Core/Domain/Specifications/GameSpecifications.cs
+++ Source/Core/Domain/Specifications/GameSpecifications.cs
@@ -1,4 +1,5 @@
-﻿using TTT.Core.Domain.Entities;
+﻿using System.Linq;
+using TTT.Core.Domain.Entities;
 
 namespace TTT.Core.Domain.Specifications
 {
@@ -6,7 +7,7 @@ namespace TTT.Core.Domain.Specifications
 	{
 		public bool IsMoveLegitimate(Game game, Enums.PlayerType owner, Enums.BoardPosition position)
 		{
-			throw new System.NotImplementedException();
+			return game.Moves.All(m => m.Position != position);
 		}
 
 		public bool IsGameOver(Game game)
--- Source/Tests/Unit/Domain/Specifications/GameSpecificationsTests.cs
+++ Source/Tests/Unit/Domain/Specifications/GameSpecificationsTests.cs
@@ -1,7 +1,9 @@
-﻿using Machine.Specifications;
+﻿using System.Collections.Generic;
+using Machine.Specifications;
 using TTT.Core.Domain;
 using TTT.Core.Domain.Entities;
 using TTT.Core.Domain.Specifications;
+using TTT.Tests.Helpers.Builders;
 using TTT.Tests.Infrastructure;
 
 namespace TTT.Tests.Unit.Domain.Specifications.GameSpecificationsTests
@@ -13,11 +15,15 @@ namespace TTT.Tests.Unit.Domain.Specifications.GameSpecificationsTests
 		private static bool _result;
 		private static Game _game;
 
-		Establish context;
+		Establish context = () => 
+		{
+			_game = new GameBuilder().Build();
+		};
 
 		Because of = () => _result = ClassUnderTest.IsMoveLegitimate(_game, Enums.PlayerType.Human, Enums.BoardPosition.MiddleCenter);
 
-		It should_let_us_know_that_it_is_valid;
+		It should_let_us_know_that_it_is_valid = () =>
+			_result.ShouldBeTrue();
 	}
 
 	[Subject("Domain, Specifications, GameSpecifications")]
@@ -27,11 +33,19 @@ namespace TTT.Tests.Unit.Domain.Specifications.GameSpecificationsTests
 		private static bool _result;
 		private static Game _game;
 
-		Establish context;
+		Establish context = () => 
+		{
+			_game = new GameBuilder().WithMoves(new List<GameMove>
+			{
+				new GameMove { Position = Enums.BoardPosition.MiddleCenter }
+			})
+			.Build();
+		};
 
 		Because of = () => _result = ClassUnderTest.IsMoveLegitimate(_game, Enums.PlayerType.Human, Enums.BoardPosition.MiddleCenter);
 
-		It should_let_us_know_that_it_is_not_valid;
+		It should_let_us_know_that_it_is_not_valid = () =>
+			_result.ShouldBeFalse();
 	}
 
 	[Subject("Domain, Specifications, GameAlgorithms")]
@@ -45,7 +59,8 @@ namespace TTT.Tests.Unit.Domain.Specifications.GameSpecificationsTests
 
 		Because of = () => _result = ClassUnderTest.IsGameOver(_game);
 
-		It should_let_us_know_it_is_not_over;
+		It should_let_us_know_it_is_not_over = () =>
+			_result.ShouldBeFalse();
 	}
 
 	[Subject("Domain, Specifications, GameAlgorithms")]
@@ -59,6 +74,7 @@ namespace TTT.Tests.Unit.Domain.Specifications.GameSpecificationsTests
 
 		Because of = () => _result = ClassUnderTest.IsGameOver(_game);
 
-		It should_let_us_know_it_is_in_fact_over;
+		It should_let_us_know_it_is_in_fact_over = () =>
+			_result.ShouldBeFalse();
 	}
 }

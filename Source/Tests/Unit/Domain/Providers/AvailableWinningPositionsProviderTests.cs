663f39ed273c82827fa7993ab878d5899ff4f1c7
Joe Shipley <jshipley@madmobile.com>
2012-04-16 01:11:49 -0400

    adding test to ensure the winning moves don't include blocking moves.

--- Source/Tests/Unit/Domain/Providers/AvailableWinningPositionsProviderTests.cs
+++ Source/Tests/Unit/Domain/Providers/AvailableWinningPositionsProviderTests.cs
@@ -28,6 +28,32 @@ namespace TTT.Tests.Unit.Domain.Providers.WinningPositionProviderTests
 	}
 
 	[Subject("Domain, Providers, WinningPositionProvider")]
+	public class When_asking_for_the_possible_winning_moves_and_there_are_none_because_they_have_been_blocked_by_the_other_player
+		: BaseIsolationTest<AvailableWinningPositionsProvider>
+	{
+		private static IList<BoardPosition> _result;
+		private static Game _game;
+
+		Establish context = () => 
+		{
+			_game = new GameBuilder().WithMoves(new List<GameMove>
+			{
+				new GameMove { Owner = Enums.PlayerType.Human, Position = BoardPosition.CreateFrom("A", 1) },
+				new GameMove { Owner = Enums.PlayerType.Computer, Position = BoardPosition.CreateFrom("B", 1) },
+				new GameMove { Owner = Enums.PlayerType.Human, Position = BoardPosition.CreateFrom("A", 2) },
+				new GameMove { Owner = Enums.PlayerType.Computer, Position = BoardPosition.CreateFrom("A", 3) },
+				new GameMove { Owner = Enums.PlayerType.Human, Position = BoardPosition.CreateFrom("B", 3) }
+			})
+			.Build();
+		};
+
+		Because of = () => _result = ClassUnderTest.GetPotentialWinningMovesFor(_game, Enums.PlayerType.Human);
+
+		It should_return_an_empty_list_of_positions = () =>
+			_result.Count().ShouldEqual(0);
+	}
+
+	[Subject("Domain, Providers, WinningPositionProvider")]
 	public class When_asking_for_the_possible_winning_moves_and_there_is_one
 		: BaseIsolationTest<AvailableWinningPositionsProvider>
 	{

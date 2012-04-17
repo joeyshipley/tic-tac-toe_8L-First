ac1a76b45ef5630cf45a411a8ce857ba3607b633
Joe Shipley <jshipley@madmobile.com>
2012-04-16 23:11:23 -0400

    adding basic styling & copy.

--- Source/Website/Helpers/HtmlExtensions.cs
+++ Source/Website/Helpers/HtmlExtensions.cs
@@ -5,19 +5,19 @@ namespace TTT.Website.Helpers
 {
 	public static class HtmlExtensions
 	{
-		public static IHtmlString Script(this HtmlHelper helper, string file) 
+		public static IHtmlString LoadScript(this HtmlHelper helper, string file) 
 		{
 			var root = VirtualPathUtility.ToAbsolute("~/Resources/Scripts");
 			return new HtmlString(string.Format("<script src=\"{0}/{1}\" type=\"text/javascript\"></script>\r\n", root, file));
 		}
 
-		public static IHtmlString Stylesheet(this HtmlHelper helper, string file) 
+		public static IHtmlString LoadStylesheet(this HtmlHelper helper, string file) 
 		{
 			var root = VirtualPathUtility.ToAbsolute("~/Resources/Styles");
 			return new HtmlString(string.Format("<link rel=\"stylesheet\" href=\"{0}/{1}\">\r\n", root, file));
 		}
 
-		public static IHtmlString Image(this HtmlHelper helper, string file) 
+		public static IHtmlString DrawImage(this HtmlHelper helper, string file) 
 		{
 			var root = VirtualPathUtility.ToAbsolute("~/Resources/Images");
 			return new HtmlString(string.Format("<img src=\"{0}/{1}\" />\r\n", root, file));

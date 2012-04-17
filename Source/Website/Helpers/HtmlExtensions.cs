5aed355634b926aeaeb732b4115f2217c4d8c9b6
Joe Shipley <jshipley@madmobile.com>
2012-04-16 21:56:41 -0400

    Adding htmlhelper extension for images.

--- Source/Website/Helpers/HtmlExtensions.cs
+++ Source/Website/Helpers/HtmlExtensions.cs
@@ -5,14 +5,22 @@ namespace TTT.Website.Helpers
 {
 	public static class HtmlExtensions
 	{
-		public static IHtmlString Script(this HtmlHelper helper, string file) {
+		public static IHtmlString Script(this HtmlHelper helper, string file) 
+		{
 			var root = VirtualPathUtility.ToAbsolute("~/Resources/Scripts");
 			return new HtmlString(string.Format("<script src=\"{0}/{1}\" type=\"text/javascript\"></script>\r\n", root, file));
 		}
 
-		public static IHtmlString Stylesheet(this HtmlHelper helper, string file) {
+		public static IHtmlString Stylesheet(this HtmlHelper helper, string file) 
+		{
 			var root = VirtualPathUtility.ToAbsolute("~/Resources/Styles");
 			return new HtmlString(string.Format("<link rel=\"stylesheet\" href=\"{0}/{1}\">\r\n", root, file));
+		}
+
+		public static IHtmlString Image(this HtmlHelper helper, string file) 
+		{
+			var root = VirtualPathUtility.ToAbsolute("~/Resources/Images");
+			return new HtmlString(string.Format("<img src=\"{0}/{1}\" />\r\n", root, file));
 		}
 	}
 }

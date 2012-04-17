33d94d4a512e5ec060771f89b99c6bf265eb5992
Joe Shipley <jshipley@madmobile.com>
2012-04-17 01:40:42 -0400

    Adding test that confirms bug exists.

---
+++ Source/Tests/Integration/Application/Services/GameServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Machine.Specifications;
+using TTT.Core.Application.Request;
+using TTT.Core.Application.Services;
+using TTT.Core.Domain.Models;
+using TTT.Tests.Infrastructure;
+
+namespace TTT.Tests.Integration.Application.Services
+{
+	[Subject("Application, Services, GameService")]
+	public class When_following_particular_move_list_that_was_resulting_in_a_bug
+		: BaseIntegrationTest<GameService>
+	{
+		private static GameModel _result;
+
+		Establish context;
+
+		Because of = () => 
+		{
+			// new game setup
+			var newGameModel = ClassUnderTest.New();
+
+			// first move to the top left
+			ClassUnderTest.PerformMove(new PerformMoveRequest { GameId = newGameModel.GameId, SelectedColumn = "C", SelectedRow = 1 });
+	
+			// second move to the top center, results in a game over error.
+			_result = ClassUnderTest.PerformMove(new PerformMoveRequest { GameId = newGameModel.GameId, SelectedColumn = "B", SelectedRow = 1 });
+		};
+
+		It should_not_return_game_over = () => 
+			_result.IsGameOver.ShouldBeFalse();
+	}
+}
